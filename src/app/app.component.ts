e686f2efe59228deeb3d3847c4085092d6b8f06a
lolotrgeek <lolotrgeek@Jonathan-Jamess-MacBook-Pro.local>
2017-07-20 11:46:38 -0500

    Notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,13 +23,21 @@ export class MyApp {
 
       // If environement is native, load native firebase
       if (platform.is('cordova')) {
+
+        console.log ('PLATFORM: Cordova')
+        
+        // FIREBASE Native
         this.firebase.getToken()
           .then(token => console.log(`The token is ${token}`)) // save the token server-side and use it to push notifications to this device
           .catch(error => console.error('Error getting token', error));
 
         this.firebase.onTokenRefresh()
           .subscribe((token: string) => console.log(`Got a new token ${token}`));
+
+        this.firebase.grantPermission()
+
       }
+
       else {
           console.log ('PLATFORM: Browser')
       }
